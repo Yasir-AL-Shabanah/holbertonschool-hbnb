3a98a52a184e31b326c833728d6e1ffe10c79c90
Hussam <samdevy@outlook.com>
2026-01-31 17:52:25 +0000

    Update place.py

--- part3/hbnb/hbnb/app/models/place.py
+++ part3/hbnb/hbnb/app/models/place.py
@@ -1,13 +1,33 @@
 from part3 import db
-from sqlalchemy import Column, String, Integer, Float
 from part3.models.base_model import BaseModel
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
+from part3.models.amenity import place_amenity
 
 
 class Place(BaseModel):
     __tablename__ = "places"
 
-    title = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
-    price = Column(Integer, nullable=False)
-    latitude = Column(Float, nullable=True)
-    longitude = Column(Float, nullable=True)
+    title = db.Column(db.String(128), nullable=False)
+    description = db.Column(db.String(1024))
+    price = db.Column(db.Integer, nullable=False)
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
+
+    owner_id = db.Column(
+        db.String(60),
+        ForeignKey("users.id"),
+        nullable=False
+    )
+
+    reviews = relationship(
+        "Review",
+        backref="place",
+        cascade="all, delete-orphan"
+    )
+
+    amenities = relationship(
+        "Amenity",
+        secondary=place_amenity,
+        back_populates="places"
+    )
