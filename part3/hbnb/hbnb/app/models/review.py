1dd40d218f36b522ce15370392595439990cd823
Hussam <samdevy@outlook.com>
2026-01-31 17:38:52 +0000

    Update review.py

--- part3/hbnb/hbnb/app/models/review.py
+++ part3/hbnb/hbnb/app/models/review.py
@@ -1,11 +1,10 @@
-from app.extensions import db
-from app.models.base_model import BaseModel
+from part3 import db
+from sqlalchemy import Column, String, Integer
+from part3.models.base_model import BaseModel
 
 
 class Review(BaseModel):
     __tablename__ = "reviews"
 
-    text = db.Column(db.Text, nullable=False)
-    rating = db.Column(db.Integer, nullable=True)
-    user_id = db.Column(db.String(36), nullable=False)
-    place_id = db.Column(db.String(36), nullable=False)
+    text = Column(String(1024), nullable=False)
+    rating = Column(Integer, nullable=False)
