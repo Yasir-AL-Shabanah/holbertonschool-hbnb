1f9e7ddb859b5d506c17433ad20538ef07ae9c34
Hussam <samdevy@outlook.com>
2026-01-31 17:53:18 +0000

    Add relationships for places and reviews in User model
    Added relationships for places and reviews in User model.

--- part3/hbnb/hbnb/app/models/user.py
+++ part3/hbnb/hbnb/app/models/user.py
@@ -1,18 +1,13 @@
-from app.extensions import db, bcrypt
-from app.models.base_model import BaseModel
+from sqlalchemy.orm import relationship
 
+places = relationship(
+    "Place",
+    backref="owner",
+    cascade="all, delete-orphan"
+)
 
-class User(BaseModel):
-    __tablename__ = "users"
-
-    first_name = db.Column(db.String(128), nullable=True)
-    last_name = db.Column(db.String(128), nullable=True)
-    email = db.Column(db.String(128), nullable=False, unique=True, index=True)
-    password_hash = db.Column(db.String(256), nullable=False)
-    is_admin = db.Column(db.Boolean, nullable=False, default=False)
-
-    def set_password(self, raw_password: str) -> None:
-        self.password_hash = bcrypt.generate_password_hash(raw_password).decode("utf-8")
-
-    def check_password(self, raw_password: str) -> bool:
-        return bcrypt.check_password_hash(self.password_hash, raw_password)
+reviews = relationship(
+    "Review",
+    backref="author",
+    cascade="all, delete-orphan"
+)
