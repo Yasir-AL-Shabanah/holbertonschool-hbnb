5608a69bac3e02b509f58fb9e17bd43e97ffc0bc
Hussam <samdevy@outlook.com>
2026-01-31 17:38:19 +0000

    Update place.py

--- part3/hbnb/hbnb/app/models/place.py
+++ part3/hbnb/hbnb/app/models/place.py
@@ -1,12 +1,13 @@
-from app.extensions import db
-from app.models.base_model import BaseModel
+from part3 import db
+from sqlalchemy import Column, String, Integer, Float
+from part3.models.base_model import BaseModel
 
 
 class Place(BaseModel):
     __tablename__ = "places"
 
-    name = db.Column(db.String(128), nullable=False)
-    description = db.Column(db.Text, nullable=True)
-    city = db.Column(db.String(128), nullable=True)
-    price_per_night = db.Column(db.Float, nullable=True)
-    host_id = db.Column(db.String(36), nullable=True)
+    title = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    price = Column(Integer, nullable=False)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
