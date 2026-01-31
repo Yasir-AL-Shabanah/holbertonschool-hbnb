437f6e278255ab24d30da4f5d2297566e852de8a
Hussam <samdevy@outlook.com>
2026-01-31 17:51:01 +0000

    Add relationship for amenities and places

--- part3/hbnb/hbnb/app/models/amenity.py
+++ part3/hbnb/hbnb/app/models/amenity.py
@@ -1,9 +1,32 @@
 from part3 import db
-from sqlalchemy import Column, String
 from part3.models.base_model import BaseModel
+from sqlalchemy.orm import relationship
+
+# Association table (Place <-> Amenity)
+place_amenity = db.Table(
+    "place_amenity",
+    db.Column(
+        "place_id",
+        db.String(60),
+        db.ForeignKey("places.id"),
+        primary_key=True
+    ),
+    db.Column(
+        "amenity_id",
+        db.String(60),
+        db.ForeignKey("amenities.id"),
+        primary_key=True
+    )
+)
 
 
 class Amenity(BaseModel):
     __tablename__ = "amenities"
 
-    name = Column(String(128), nullable=False, unique=True)
+    name = db.Column(db.String(128), nullable=False, unique=True)
+
+    places = relationship(
+        "Place",
+        secondary=place_amenity,
+        back_populates="amenities"
+    )
