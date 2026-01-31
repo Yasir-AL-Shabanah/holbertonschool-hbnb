8bd5399d38fddfb486b4d505e332dc89f6cdbcc2
Hussam <samdevy@outlook.com>
2026-01-31 17:52:50 +0000

    Update review.py

--- part3/hbnb/hbnb/app/models/review.py
+++ part3/hbnb/hbnb/app/models/review.py
@@ -1,10 +1,22 @@
 from part3 import db
-from sqlalchemy import Column, String, Integer
 from part3.models.base_model import BaseModel
+from sqlalchemy import ForeignKey
 
 
 class Review(BaseModel):
     __tablename__ = "reviews"
 
-    text = Column(String(1024), nullable=False)
-    rating = Column(Integer, nullable=False)
+    text = db.Column(db.String(1024), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+
+    place_id = db.Column(
+        db.String(60),
+        ForeignKey("places.id"),
+        nullable=False
+    )
+
+    user_id = db.Column(
+        db.String(60),
+        ForeignKey("users.id"),
+        nullable=False
+    )
