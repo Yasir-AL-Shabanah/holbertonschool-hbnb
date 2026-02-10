bb391bf0a3c8341bd4d69040127ae6cff594ae37
Yasir-AL-Shabanah <Yaser.al.shabanah@gmail.com>
2026-02-10 20:07:48 +0000

    Final Prep for Part 4: Fix CORS, Password Security, and DB Relations

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_cors import CORS
+from config import config
+
+def create_app(config_name='default'):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    
+    # تفعيل CORS
+    CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+    
+    # يفضل تسجيل الـ Blueprints هنا لاحقاً إذا لم تكن مسجلة
+    # from app.api.v1.users import users_bp
+    # app.register_blueprint(users_bp)
+    
+    return app
